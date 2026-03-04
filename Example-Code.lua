c7bf76fdd83ab7704b930e1fc20ebfa3078ec2e5
Xeno-Automation <imakeaiagentsawman@gmail.com>
2026-03-04 17:07:02 -0600

    Update Example-Code.lua

--- Example-Code.lua
+++ Example-Code.lua
@@ -1,12 +1,12 @@
 getgenv().TargetList = {
-	[1] = {
+	[1] = { -- step 1
 		Type = "Click",
 		Path = game:GetService("Players").LocalPlayer.PlayerGui.Clicker.["Bounding Box"],
 		Target = "TextButton",
 		ClickDelay = 0.01,
 		AfterDelay = 0,
 	},
-	[2] = {
+	[2] = { -- step 2
 		Type = "Typing",
 		Path = game:GetService("Players").LocalPlayer.PlayerGui.Typing["Bounding Box"],
 		Target = "TextBox",
@@ -19,10 +19,14 @@ getgenv().TargetList = {
 			RandomThinkDelay = 1,
 		}
 	},
+	-- add more numbered tables if you want to add multiple steps. The last task goes back to the first task when the afterdelay is finished.
 }
 
+-- toggles the script (not pause, end and then restart)
 getgenv().ToggleKey = Enum.KeyCode.K
+-- kills the script, and will not come back until you re-execute
 getgenv().KillKey = Enum.KeyCode.L
+-- the random chance to pause and think during typing
 getgenv().RandomThinkChance = 25
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Xeno-Automation/Xeno-Automation-Tool/refs/heads/main/main.lua"))()
